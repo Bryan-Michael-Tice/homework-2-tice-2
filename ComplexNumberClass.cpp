4b33391aa5735b32d75254d14431f48014b346a6
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-25 19:52:03 -0700

    deleted the jargon we got in our code at the cost of losing a few functions :'(

--- ComplexNumberClass.cpp
+++ ComplexNumberClass.cpp
@@ -19,40 +19,6 @@ Complex::Complex(double real, double imag){
 	_real = new double;
 	_imag = new double;
 }
-Complex Complex::add(double &number1, double &number2){
-	Complex c = {c1.real+c2.real, c1.imag+c2.imag};
-	return c;
-}
-<<<<<<< HEAD
-typedef struct ComplexAdd Complex;
-Complex ComplexSub(Complex c1, Complex c2){
-=======
-
-Complex Complex::sub(Complex c1, Complex c2){
->>>>>>> 0e98d2dd10f6952c78825b1b55e4fc6bdef438dc
-	Complex c = {c1.real-c2.real, c1.imag-c2.imag};
-	return c;
-}
-Complex Complex::mult(Complex c1, Complex c2){
-	Complex c = {(c1.real*c2.real)+(c1.imag*c2.imag*-1),(c1.imag*imag*c2.real)+(c1.real*c2.imag)};
-	return c;
-}
-
-<<<<<<< HEAD
-
-Complex ComplexDiv(Complex c1, Complex c2){
-=======
-Complex Complex::div(Complex c1, Complex c2){
->>>>>>> 0e98d2dd10f6952c78825b1b55e4fc6bdef438dc
-	double denominator = ComplexMulti(c2)*ComplexMulti(c2);
-	Complex c = ComplexMult(c1,ComplexConj(c2));
-
-	while(denominator !=0){
-	c.real /= denominator;
-	c.imag /= denominator;
-	return c;	
-	}
-}
 
 
 Complex Complex::conj(Complex c){
