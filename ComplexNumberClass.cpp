0e98d2dd10f6952c78825b1b55e4fc6bdef438dc
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-25 19:26:43 -0700

    fixed function names to match main.c and put in the ::'s

--- ComplexNumberClass.cpp
+++ ComplexNumberClass.cpp
@@ -10,21 +10,21 @@
  *
  */
 
-Complex ComplexAdd(double &number1, double &number2){
+Complex Complex::add(double &number1, double &number2){
 	Complex c = {c1.real+c2.real, c1.imag+c2.imag};
 	return c;
 }
 
-Complex ComplexSub(Complex c1, Complex c2){
+Complex Complex::sub(Complex c1, Complex c2){
 	Complex c = {c1.real-c2.real, c1.imag-c2.imag};
 	return c;
 }
-Complex ComplexMulti(Complex c1, Complex c2){
+Complex Complex::mult(Complex c1, Complex c2){
 	Complex c = {(c1.real*c2.real)+(c1.imag*c2.imag*-1),(c1.imag*imag*c2.real)+(c1.real*c2.imag)};
 	return c;
 }
 
-Complex ComplexDiv(Complex c1, Complex c2){
+Complex Complex::div(Complex c1, Complex c2){
 	double denominator = ComplexMulti(c2)*ComplexMulti(c2);
 	Complex c = ComplexMult(c1,ComplexConj(c2));
 
@@ -36,7 +36,7 @@ Complex ComplexDiv(Complex c1, Complex c2){
 }
 
 
-Complex CompexConj(Complex c){
+Complex Complex::conj(Complex c){
 	if(*c.imag<0){
 		std::cout<< " " <<c.real<<"  " << -c.imag;
 		std::cout<<endl;
@@ -46,11 +46,11 @@ Complex CompexConj(Complex c){
 	}
 }
 
-double ComplexMag(const Complex c){
+double Complex::mag(const Complex c){
 	return sqrt((c.real*c.real)+(c.imag*c.imag));
 }
 
-double ComplexPhase(const Complex c){
+double Complex::phase(const Complex c){
 	double phase= atan(c.imag/c.real)
 	phase = phase (180/M_PI);
 	return (phase<0) ? -ph : ph;
