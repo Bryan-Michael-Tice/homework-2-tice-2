31e5181b2df4bdce166ca19f1a8c0f2ac3977553
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-25 19:02:21 -0700

    updated .hpp to resemble coding standard better and put in friend statements for std ostream / istream

--- MyClass.hpp
+++ MyClass.hpp
@@ -6,35 +6,30 @@ class Complex{
 	private:
 	double _real;
 	double _imag;
+       
+	void reccopy(const double *ptr);
 
- void recopy(const double *ptr);
-};
 
 	public:
-	
-Complex();
-Complex(double real, double imag){
-	_real = real;
-	_imag = imag; 
-	}
-Complex(const Complex &number){
-	_real = number._real;
-	_imag = number._imag;
-}
-	~Complex(){
-}
-double RealNumGetter() const;
-double ImagNumGetter() const;
+		Complex();
+		Complex(double real, double imag);
+		Complex(const Complex &number);
+		~Complex();
+		
+		double RealNumGetter() const;
+		double ImagNumGetter() const;
+		
+		void print();
+		
+		void ComplexAdd(Complex c1, Complex c2);
+		void ComplexSub(Complex c1, Complex c2);
+		void ComplexMulti(Complex c1, Complex c2);
+		void ComplexDiv(Complex c1, Complex c2);
+		void CompexConj(Complex c);
+		void ComplexMag(Complex c);
+		void ComplexPhase(Complex c);
 
-void print();
-
-void ComplexAdd(Complex c1, Complex c2);
-void ComplexSub(Complex c1, Complex c2);
-void ComplexMulti(Complex c1, Complex c2);
-void ComplexDiv(Complex c1, Complex c2);
-void CompexConj(Complex c);
-void ComplexMag(Complex c);
-
-void ComplexPhase(Complex c);
-
-
+ friend std::ostream &operator<<(std::iostream & out, Complex &complex_int);
+ 
+ friend std::istream &operator>>(std::istream & out, Complex &complex_int);
+ };
