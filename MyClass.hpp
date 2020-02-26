b2967b3c1c7a8e2ae926ada8f5eeb2f4e366a56b
pjtic <pauly4iron@gmail.com>
2020-02-25 21:54:00 -0700

    changed data type to one inside prototype.

--- MyClass.hpp
+++ MyClass.hpp
@@ -13,7 +13,7 @@ class Complex{
 	public:
 		Complex();
 		Complex(double real, double imag);
-		Complex(const Complex double);
+		Complex(const double);
 		~Complex();
 		
 		double RealNumGetter() const;
