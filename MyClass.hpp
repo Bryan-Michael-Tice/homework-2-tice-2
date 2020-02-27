b7df301955a88c57ddc661d8d2d22eaf0d43ad76
Bryan-Michael-Tice <bryan.michael.tice@gmail.com>
2020-02-26 20:42:24 -0700

    debugged more stuff

--- MyClass.hpp
+++ MyClass.hpp
@@ -29,11 +29,11 @@ class Complex{
 		double magnitude();
 		double phase();
 
-		Complex operator+(Complex&);
-		Complex operator-(Complex&);		
-		Complex operator*(Complex&);
-		Complex operator/(Complex&);
-		Complex operator=(Complex&);
+		Complex operator+(Complex value);
+		Complex operator-(Complex value);		
+		Complex operator*(Complex value);
+		Complex operator/(Complex value);
+		Complex operator=(Complex value);
 
  friend std::ostream& operator<<(std::ostream &out, const Complex &C1);
  
